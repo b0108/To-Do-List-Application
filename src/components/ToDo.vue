e02f45bfa3dc33a75396259e1ec5c0126e52f97f
Bibobra <bibobra000@gmail.com>
2025-07-18 16:45:29 +0100

    fix edit and delete buttons

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -73,10 +73,39 @@
       >
         {{ todo.dueDate }}
       </el-tag>
-      <el-button-group>
-        <el-button icon="el-icon-edit" @click="editTodoItem(idx)" />
-        <el-button icon="el-icon-delete" @click="deleteTodoItem(idx)" />
-      </el-button-group>
+      <div class="todo-actions">
+        <button class="icon-button" @click="editTodoItem(idx)">
+          <svg
+            fill="var(--secondary-color)"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+          >
+            <path
+              d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"
+            />
+          </svg>
+        </button>
+
+        <button
+          id="delete-btn"
+          class="icon-button"
+          @click="deleteTodoItem(idx)"
+        >
+          <svg
+            fill="var(--secondary-color)"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path
+              d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   </el-card>
 </template>
@@ -262,9 +291,34 @@ h2 {
   color: var(--text-color);
 }
 
+.icon-button {
+  background: none;
+  border: none;
+  padding: 4px;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+}
+
+.icon-button:hover {
+  transform: scale(1.1);
+  background-color: var(--accent-color);
+}
+
+#delete-btn:hover {
+  background-color: var(--danger-color);
+  transform: scale(1.1);
+}
+
+.todo-text:hover {
+  cursor: pointer;
+}
+
 /* Tag Styling */
 ::v-deep(.el-tag.el-tag--info) {
-  background-color: var(--accent-color);
   color: var(--primary-color);
   border: none;
 }
